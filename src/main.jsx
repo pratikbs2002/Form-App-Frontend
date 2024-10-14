1adc548e6dec64aaa932a78aca910bbb8e3ad85a
pratikbs2002 <pratiksuthar1020@gmail.com>
2024-10-14 14:51:13 +0530

    ADD: Loader Provider Context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthProvider.jsx";
+import { LoaderContext } from "./context/LoaderProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <LoaderContext>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </LoaderContext>
   // </StrictMode>,
 );
